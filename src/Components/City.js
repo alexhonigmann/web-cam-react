516e22893e283ff50e6e88721c55f24deb935d32
Marcin <mgabrys422@gmail.com>
2020-04-09 12:56:52 +0200

    webcame

--- src/Components/City.js
+++ src/Components/City.js
@@ -3,34 +3,46 @@ import "./city.css"
 import citydata from "./citydata.js"
 
 class City extends Component {
-    state = { 
+    state = {
         description: "",
         selectedCity: "",
-        isChecked: false
-     }
+        isChecked: false,
+        video: ""
+    }
 
     // handleClick = (city) => {
     //     this.setState({ description: citydata[city].description });
     //     this.setState({ selectedCity: city });
 
-        handleClick = (city) => {
-            this.setState({ description: citydata[city].description });
-            this.setState({ selectedCity: citydata[city].name });
-            this.setState({ isChecked: true });
+    handleClick = (city) => {
+        this.setState({ description: citydata[city].description });
+        this.setState({ selectedCity: citydata[city].name });
+        this.setState({ isChecked: true });
     }
-    
+
     handleBack = () => {
         this.setState({ description: "" });
         this.setState({ selectedCity: "" });
         this.setState({ isChecked: false });
-        }
+    }
 
+    componentDidMount() {
+        fetch('https://api.windy.com/api/webcams/v2/list/country=DE?key=0g3HlpB7OECc0g5zoQU70Pc5SktXe8Kj')
+            .then((response) => {
+                return response.json();
+            })
 
-    render() { 
+            .then((data) => {
+                console.log(data);
+                //  this.setState({ news: newArray });
+            })
+    };
+
+    render() {
         // return ( 
         //     <div className="city-container">
         //     <ul>
-                
+
         //         <li onClick={() => this.handleClick(0)}>New York</li>
         //         <li onClick={() => this.handleClick(1)}>London</li>
         //         <li onClick={() => this.handleClick(2)}>Paris</li>
@@ -47,24 +59,24 @@ class City extends Component {
         //  );
 
 
-         return (
+        return (
             <div className="city-container">
-            <ul style={{ display: this.state.isChecked ? 'none' : 'block' }} >
-            <li onClick={() => this.handleClick(0)}>New York</li>
-            <li onClick={() => this.handleClick(1)}>London</li>
-            <li onClick={() => this.handleClick(2)}>Paris</li>
-            <li onClick={() => this.handleClick(3)}>Berlin</li>
-            </ul>
-            <div>
-            <button style={{ display: !this.state.isChecked ? 'none' : 'block' }} onClick={this.handleBack}>back</button>
-            {this.state.selectedCity}
-            {this.state.description}
-            </div>
+                <ul style={{ display: this.state.isChecked ? 'none' : 'block' }} >
+                    <li onClick={() => this.handleClick(0)}>New York</li>
+                    <li onClick={() => this.handleClick(1)}>London</li>
+                    <li onClick={() => this.handleClick(2)}>Paris</li>
+                    <li onClick={() => this.handleClick(3)}>Berlin</li>
+                </ul>
+                <div>
+                    <button style={{ display: !this.state.isChecked ? 'none' : 'block' }} onClick={this.handleBack}>back</button>
+                    {this.state.selectedCity}
+                    {this.state.description}
+                </div>
             </div >
-            );
+        );
     }
 }
- 
+
 export default City;
- 
+
 
